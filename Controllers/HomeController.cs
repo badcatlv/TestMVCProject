7ca071ff996d50969458314e5503a5a7889dee0a
Colleen Blake <colleenemilyblake@gmail.com>
2024-06-09 18:28:22 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using EComMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EComMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/EComDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EComMVCProject.Data
+{
+    public class EComDbContext : IdentityDbContext
+    {
+        public EComDbContext(DbContextOptions<EComDbContext> options) : base(options)
+        {
+        }
+
+        
+    }
+}
