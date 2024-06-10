5db0bb610e6c2c62fa02e87dc63e7a7b3450c34b
Colleen Blake <colleenemilyblake@gmail.com>
2024-06-09 19:48:32 -0500

    Create Product model and add to database.

--- Data/EComDbContext.cs
+++ Data/EComDbContext.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using EComMVCProject.Models;
 
 namespace EComMVCProject.Data
 {
     public class EComDbContext : IdentityDbContext
     {
+        public DbSet<Product> Products { get; set; }
+        
         public EComDbContext(DbContextOptions<EComDbContext> options) : base(options)
         {
         }
 
-        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+        }
+
+
     }
 }
---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EComMVCProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Precision(18,2)]
+        public decimal Price { get; set; }
+
+        public Product(int id, string name, decimal price, string description)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Description = description;
+
+        }
+
+        public Product() { }
+
+
+    }
+}
